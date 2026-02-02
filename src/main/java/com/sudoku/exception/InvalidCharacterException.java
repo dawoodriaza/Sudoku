1879f8f6429514848c625a81190ab3b64917bd13
dawoodriaza <od.riaz@alsalambank.com>
2026-02-02 19:10:16 +0300

    Created two extensions

---
+++ src/main/java/com/sudoku/exception/InvalidCharacterException.java
@@ -0,0 +1,7 @@
+package com.sudoku.exception;
+
+public class InvalidCharacterException extends Exception {
+    public InvalidCharacterException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/sudoku/exception/SudokuFileNotFoundException.java
@@ -0,0 +1,7 @@
+package com.sudoku.exception;
+
+public class SudokuFileNotFoundException extends Exception {
+    public SudokuFileNotFoundException(String message) {
+        super(message);
+    }
+}
