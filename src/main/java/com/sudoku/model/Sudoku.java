ee8f8259285fa06dab284f02136646687fd251cb
dawoodriaza <od.riaz@alsalambank.com>
2026-02-04 18:08:43 +0300

    Created Solver nd controller

--- src/main/java/com/sudoku/model/Sudoku.java
+++ src/main/java/com/sudoku/model/Sudoku.java
@@ -16,22 +16,6 @@ public class Sudoku {
         }
     }
 
-    public void printGridsBoard() {
-        for (int i = 0; i < 9; i++) {
-            if (i % 3 == 0 && i != 0) {
-                System.out.println("----------------------------------");
-            }
-            for (int j = 0; j < 9; j++) {
-                if (j % 3 == 0 && j != 0) {
-                    System.out.print(" | ");
-                }
-                int value = grids[i][j].getValue();
-                System.out.print(" " + value + " ");
-            }
-            System.out.println();
-        }
-        System.out.println("\n");
-    }
 
     public String getGridAsString() {
         StringBuilder bord = new StringBuilder();
